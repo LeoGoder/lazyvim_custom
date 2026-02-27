81f5aa49c43f4b400b87563a5d704416e387d7d8
Leo Goderneaux <lgoderne@z2r9p6.42lyon.fr>
2026-02-27 19:02:25 +0100

    adsfasdf

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
+-- 0: jamais de barre, 1: seulement si split, 2: toujours, 3: barre unique (global)
+vim.opt.laststatus = 2 
+
+-- Pour afficher le mode (-- INSERT --, -- VISUAL --) en bas à gauche
+vim.opt.showmode = true
